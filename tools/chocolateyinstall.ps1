6d608d3365fc1e165b1559e357a908c718d83265
Franco Biasin <franconbiasin@gmail.com>
2020-10-27 20:22:14 -0300

    Remove comments from chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,21 +1,21 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'droidcam'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version = '6.3.3'
-$url = "https://files.dev47apps.net/win/DroidCam.Setup.$version.exe" # download url, HTTPS preferred
-$url64 = $url # 64bit URL here or remove - if installer is both, use $url
+$url = "https://files.dev47apps.net/win/DroidCam.Setup.$version.exe"
+$url64 = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe'
   url           = $url
   url64bit      = $url64
 
   softwareName  = 'Droidcam Client'
 
-  silentArgs   = '/S' # NSIS
+  silentArgs   = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Install-ChocolateyPackage @packageArgs
